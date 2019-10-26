0482b618d8123da5c3effa874ee6339294380942
Parker Cooper <parkr.coopr@gmail.com>
2019-10-26 12:38:48 -0400

    posts, user authentication

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
+const findIndex = (arr, field, reference) => {
+    return arr.findIndex((element) => { element[field] === reference[field] }
+)}
+
+const resolvers = {
+    Query,
+    Mutation,
+    User,
+    Link
+  }
+
+  const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context: request => {
+      return {
+        ...request,
+        prisma,
+      }
+    },
+  })
+server.start(()=> console.log('Server listening on port 4000'))
